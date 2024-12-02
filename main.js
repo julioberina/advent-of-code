1066617aa5497ccf63ae090c07cd37daad5fda74
Julio Berina <julioberina@mac.lan>
2024-12-01 22:10:06 -0800

    Remove lingering await for calling part1 and part2 functions

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import * as d from './2024/day2/day2.js';
 
 console.log('Advent of Code, Day 2\n');
 console.log('************************\n');
-console.log('Part 1 Answer: ', await d.part1());
-console.log('Part 2 Answer: ', await d.part2());
+console.log('Part 1 Answer: ', d.part1());
+console.log('Part 2 Answer: ', d.part2());
 console.log('\n************************\n');
