fc90666dbf56140dd319bcb6e0363fee0084c40a
Julio Berina <julioberina@mac.lan>
2024-12-08 16:54:31 -0800

    Solve Day 8

---
+++ 2024/day8/day8.js
@@ -0,0 +1,83 @@
+import * as util from '../../utility.js';
+
+const filePath = '/2024/day8/input.txt';
+const data = (await util.readFile(filePath))
+  .map(line => line.split(''));
+const rowCount = data.length;
+const colCount = data[0].length;
+const antinodeSpots = {};
+
+const isSpotInBounds = (i, j) => {
+  return (i >= 0 && i < rowCount && j >= 0 && j < colCount);
+}
+
+const isWithinOne = (i1, j1, i2, j2) => {
+  return (Math.abs(j2 - j1) + Math.abs(i2 - i1)) <= 1;
+}
+
+export const part1 = () => {
+  const uniqueAntinodeLocations = new Set();
+
+  for (let i = 0; i < rowCount; ++i) {
+    for (let j = 0; j < colCount; ++j) {
+      if (data[i][j] === '.') { continue; }
+
+      const char = data[i][j];
+      if (!antinodeSpots[char]) { antinodeSpots[char] = [[i, j]]; }
+      else { antinodeSpots[char].push([i, j]); }
+    }
+  }
+
+  for (const antinode of Object.keys(antinodeSpots)) {
+    const an = antinodeSpots[antinode].length;
+
+    for (let i = 0; i < an; ++i) {
+      const x = antinodeSpots[antinode][i];
+
+      for (let j = 0; j < an; ++j) {
+        if (i === j) { continue; }
+        const y = antinodeSpots[antinode][j];
+
+        if (isWithinOne(x[0], x[1], y[0], y[1])) { continue; }
+
+        const [dx, dy] = [y[0] - x[0], y[1] - x[1]];
+
+        if (isSpotInBounds(y[0] + dx, y[1] + dy)) {
+          uniqueAntinodeLocations.add(`${y[0] + dx},${y[1] + dy}`);
+        }
+      }
+    }
+  }
+
+  return uniqueAntinodeLocations.size;
+}
+
+export const part2 = () => {
+  const uniqueAntinodeLocations = new Set();
+
+  for (const antinode of Object.keys(antinodeSpots)) {
+    const an = antinodeSpots[antinode].length;
+
+    for (let i = 0; i < an; ++i) {
+      const x = antinodeSpots[antinode][i];
+      uniqueAntinodeLocations.add(`${x[0]},${x[1]}`);
+
+      for (let j = 0; j < an; ++j) {
+        if (i === j) { continue; }
+        const y = antinodeSpots[antinode][j];
+        uniqueAntinodeLocations.add(`${y[0]},${y[1]}`);
+
+        const [dx, dy] = [y[0] - x[0], y[1] - x[1]];
+        let spotX = y[0] + dx, spotY = y[1] + dy;
+
+        while (isSpotInBounds(spotX, spotY)) {
+          uniqueAntinodeLocations.add(`${spotX},${spotY}`);
+          spotX += dx;
+          spotY += dy;
+        }
+      }
+    }
+  }
+  
+  return uniqueAntinodeLocations.size;
+}
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as d from './2024/day7/day7.js';
+import * as d from './2024/day8/day8.js';
 
-console.log('Advent of Code, Day 7\n');
+console.log('Advent of Code, Day 8\n');
 console.log('************************\n');
 console.log('Part 1 Answer: ', d.part1());
 console.log('Part 2 Answer: ', d.part2());
